5231422886792d0ad5f16f5bc191a4e64cc0922f
Student <example@example.com>
2019-10-22 14:54:05 -0700

    create a new flashcards website

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $(".card").click(function(){
+    $(".hide").toggle();
+  });
+  $(".card1").click(function(){
+    $(".hide1").toggle();
+  });
+  $(".card2").click(function(){
+    $(".hide2").toggle();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
